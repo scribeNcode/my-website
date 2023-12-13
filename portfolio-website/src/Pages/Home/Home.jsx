34b6fe649a045a3b07bf1a665f39fb14d0915bc1
scribeNcode <deyeye99@gmail.com>
2023-12-13 20:53:29 +0100

    add my image and style home page title

--- portfolio-website/src/Pages/Home/Home.jsx
+++ portfolio-website/src/Pages/Home/Home.jsx
@@ -1,15 +1,28 @@
-import "../Home/Home.css"
+import "../Home/Home.css";
+import image from "./Images/myImage.png"
 
 function Home() {
   return (
     <div className="home-page-wrapper">
       <div className="intro-container">
-        <div className="my-image-wrapper"></div>
+      <div className="my_image">
+              <img src={image} alt="my" />
+            </div>
+        {/* <div className="my-image-wrapper"></div> */}
         <div className="short-intro-about-me">
-          <p><a href="">I am Boluwatife Adeyeye</a> (scribeNcode). A frontend developer. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit asperiores aut reprehenderit quia a, quidem quo dolores doloremque aperiam temporibus iure enim, quod atque! Laudantium veritatis voluptas quas sunt dolores!</p>
+          <h2>
+            I Build <span className="digital">Scalable Digital</span> Products
+          </h2>
+          <p>
+            <a href="">I am Boluwatife Adeyeye</a> (scribeNcode). A frontend
+            developer. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Sit asperiores aut reprehenderit quia a, quidem quo dolores
+            doloremque aperiam temporibus iure enim, quod atque! Laudantium
+            veritatis voluptas quas sunt dolores!
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
-export default Home
+export default Home;
