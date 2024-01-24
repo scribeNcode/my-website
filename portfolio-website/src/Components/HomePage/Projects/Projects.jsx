7852b49d8f2a2347ae95bb0ae896b29b97cfba1c
scribeNcode <deyeye99@gmail.com>
2024-01-24 11:44:21 +0100

    add project title

--- portfolio-website/src/Components/HomePage/Projects/Projects.jsx
+++ portfolio-website/src/Components/HomePage/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import "../Projects/Projects.css"
 
 function Projects() {
   return <div className="projects-wrapper">
+    <h1>MY PORTFOLIOS</h1>
     <div className="project-img-div"></div>
     <div className="project-description"></div>
     </div>
